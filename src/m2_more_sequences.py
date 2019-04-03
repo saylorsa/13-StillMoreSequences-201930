dba64411b78e1482061a554905db989a166fbbad
saylorsa <saylorsa@rose-hulman.edu>
2019-04-03 10:39:27 -0400

    m2, Done - 3

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -188,7 +188,7 @@ def index_of_largest_number(numbers, n):
 
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
     maxdex = 0
