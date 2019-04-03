f7ab64829fd4d51892fe8fd3d3700fca4f8cabd9
saylorsa <saylorsa@rose-hulman.edu>
2019-04-03 10:40:04 -0400

    m2, Done - 4

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -249,7 +249,7 @@ def number_of_stutters(s):
        :type s: str
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
 
