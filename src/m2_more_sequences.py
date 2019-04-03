5f3619e5d5f9cf64d10f6238ea0cb8496e4ccbe3
saylorsa <saylorsa@rose-hulman.edu>
2019-04-03 10:40:45 -0400

    m2, Done - 6

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -403,7 +403,7 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
     count = 0
